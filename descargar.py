806feeb59c41c3e18ca763b19690c0b29e1ff12f
roycristhopher <yoroicode@gmail.com>
2023-01-26 13:25:11 -0500

    cl: :construction_worker: add descargar playlist button

--- descargar.py
+++ descargar.py
@@ -38,11 +38,14 @@ def descargarplaylist():
 #VENTANA GENERAL
 ventana = Tk()
 ventana.config(bd=15)
-ventana.title("Descargar Videos de Youtube")
+ventana.title("Youtube Downloader")
+
+ventana.grid_columnconfigure(2, pad=50)
+ventana.grid_rowconfigure(1, pad=50)
 
 #ENTRADA DE TEXTO
 videos = Entry(ventana)
-videos.grid(row=1, column=2)
+videos.grid(row=1, column=0)
 
 #BARRA SUPERIOR
 menubar = Menu(ventana)
@@ -55,25 +58,34 @@ menubar.add_command(label="Salir", command=ventana.destroy)
 #IMAGEN
 imagen = PhotoImage(file="img/youd.png")
 foto = Label(ventana, image=imagen)
-foto.grid(row=0, column=0)
+#foto.grid(row=0, column=1)
 
 #barra de descarga
 #progressbar = Progressbar(ventana, length=200, maximum=100, mode="determinate")
 #progressbar.grid(row=4, column=0)
 
 #INFO
-info = Label(ventana, text="VIDEO: ")
-info.grid(row=1, column=0)
+info = Label(ventana, text="INFORMACIÓN: ")
+info.grid(row=1, column=2)
 
 #INSTRUCCIONES
-instrucciones = Label(ventana, text="Ingrese Link")
-instrucciones.grid(row=0, column=2)
 
-#BOTON
+pegarlinkl = Label(ventana, text="PEGAR LINK")
+pegarlinkl.grid(row=0, column=0)
+
+instrucciones = Label(ventana, text="Para video")
+instrucciones.grid(row=3, column=0)
+
+instrucciones2 = Label(ventana, text="Para Playlist")
+instrucciones2.grid(row=5, column=0)
+
+#BOTONES
 boton = Button(ventana, text="DESCARGAR", command=descargar)
-boton.grid(row=2, column=2)
+boton.grid(row=4, column=0)
 
 botonplaylist = Button(ventana, text="DESCARGAR PLAYLIST", command=descargarplaylist)
-botonplaylist.grid(row=4, column=2)
+botonplaylist.grid(row=6, column=0)
 
 ventana.mainloop()
+
+#revisar DearPyGUI y FLET para cambiar el GUI
